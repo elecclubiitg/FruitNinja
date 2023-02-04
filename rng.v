9378f3b905a9150b0f656a68bd6f4a82d89a1a11
KBdeGaulle1803 <112582705+KBdeGaulle1803@users.noreply.github.com>
2023-02-04 23:08:29 +0530

    Update rng.v
    Module for generating random numbers for initial launch coordinates and angles. Updated version which was used for testing and implemented on FPGA.

--- rng.v
+++ rng.v
@@ -1,27 +1,48 @@
 `timescale 1ns / 1ps
+
 module rng(
-  output reg [4:0] curr_num, stored_increment, stored_modulo,
-  output reg [4:0] stored_multiplier,
-  output reg start= 1'b0,
-  input [4:0] seed, modulo, increment,
-  input [4:0] multiplier,
-  input clock, reset
+output [6:0] dig,
+output [7:0] an,
+input new, reset, ck, on
 );
-  
-  always@(posedge clock)
-    begin
-      if(!start)
-        start<= ~start;
-      curr_num<= ((stored_multiplier*curr_num)+stored_increment)%stored_modulo;
-    end
-  
-  always@(posedge reset, posedge start)
-    begin
-      stored_increment<= increment;
-      stored_multiplier<= multiplier;
-      stored_increment<= increment;
-      stored_modulo<= modulo;
-      curr_num<= seed;
-    end
-  
+reg [4:0] num=5'd1;
+reg [4:0] ctr=0, seed=5'd1, mult=5'd5, inc=5'd7;
+reg prevnew=0, prevres=0;
+wire ck2, ck3;
+
+clk_wiz_0 clkgen(
+ck2, ck3, ck 
+);
+
+always@(posedge ck2)
+begin
+if(on)begin
+if(!prevres && reset)begin
+{num, seed, mult, inc, ctr}={5'd1, 5'd1, 5'd5, 5'd7, 5'd0};
+end else if(!prevnew && new) begin
+if(ctr==5'd31) begin
+ctr=5'd0;
+seed=(seed==5'd31)?5'd1:seed+1;
+mult=(mult==5'd29)?5'd5:mult+5'd4;
+inc[0]=1;
+inc[1] = (~seed[4] & ~seed[3]) | (~seed[2] & ~seed[1] & seed[0]) | (~seed[2] & seed[1] & ~seed[0]) | (seed[2] & seed[1] & seed[0]) | (seed[4] & ~seed[2] & ~seed[1]) | (seed[4] & seed[3] & ~seed[1] & seed[0]);
+inc[2] = (~seed[4] & ~seed[3] & ~seed[2]) | (seed[4] & ~seed[3] & seed[2]) | (seed[4] & seed[2] & seed[1]) | (~seed[4] & ~seed[3] & ~seed[1] & ~seed[0]) | (seed[3] & ~seed[2] & ~seed[1] & seed[0]) | (~seed[3] & ~seed[2] & seed[1] & seed[0]) | (seed[4] & seed[3] & ~seed[2] & ~seed[0]);
+inc[3] = (~seed[4] & ~seed[0]) | (~seed[3] & ~seed[0]) | (~seed[2] & ~seed[0]) | (seed[4] & seed[3] & seed[1]) | (seed[4] & seed[3] & seed[2] & seed[0]);
+inc[4] = (~seed[4] & ~seed[3] & seed[1] & seed[0]) | (~seed[4] & ~seed[3] & ~seed[1] & ~seed[0]) | (seed[3] & seed[2] & ~seed[1] & seed[0]) | (seed[3] & seed[1] & ~seed[0]) | (seed[4] & ~seed[1] & seed[0]) | (seed[4] & seed[1] & ~seed[0]) | (seed[4] & seed[3] & seed[2]) | (~seed[4] & ~seed[2] & ~seed[1] & ~seed[0]);
+num=seed;
+end else begin
+ctr=ctr+1;
+num=((mult*num)+inc)%6'd32;
+end
+end
+end
+else begin
+{num, seed, mult, inc, ctr}={5'd1, 5'd1, 5'd5, 5'd7, 5'd0};
+end
+prevres= reset;
+prevnew= new;
+end
+
+seg convert(dig, an, num, ck3);
+
 endmodule
